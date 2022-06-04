3667b3b4e0d2d25157a94e8666e0ab5b5b967bd8
JosuéCh06 <josuechupica06@gmail.com>
2022-06-03 20:18:49 -0500

    Mejoras en las plantillas

--- src/main/java/com/chinafood/cliente/controller/PlatilloController.java
+++ src/main/java/com/chinafood/cliente/controller/PlatilloController.java
@@ -1,19 +1,14 @@
 package com.chinafood.cliente.controller;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.chinafood.cliente.entity.CategoriaPlatillo;
@@ -43,43 +38,23 @@ public class PlatilloController {
 	}
 
 	@RequestMapping("/grabar")
-	public String grabar(@RequestParam("descripcion") String desc, @RequestParam("codigo") int cod,
-			@RequestParam("precio") double precio, @RequestParam("categoria") int categoria,
-			@RequestParam("imagen") MultipartFile file, RedirectAttributes redirect) {
+	public String grabar(@ModelAttribute Platillo p, RedirectAttributes redirect) {
 		try {
-			String folder = "src//main//resources//static//resources//cargas//platillos//";
-			if (!file.isEmpty()) {
-				try {
-					Platillo bean = new Platillo();
-					bean.setDescripcion(desc);
-					bean.setPrecio(precio);
-					bean.setCategoria(new CategoriaPlatillo(categoria));
-					String ruta = "/resources/cargas/platillos/";
-
-					byte[] bytes = file.getBytes();
-					Path path = Paths.get(folder + file.getOriginalFilename());
-					Files.write(path, bytes);
-
-					bean.setLinkImagen(ruta + file.getOriginalFilename());
-
-					// Serializar
-					Gson gson = new Gson();
-					String json = gson.toJson(bean);
-					//
-					RestTemplate rt = new RestTemplate();
-					HttpHeaders headers = new HttpHeaders();
-					headers.setContentType(MediaType.APPLICATION_JSON);
-					HttpEntity<String> request = new HttpEntity<>(json, headers);
-					if (bean.getIdPlatillo() != 0) {
-						rt.put(URL + "platillo/actualizar", request, String.class);
-						redirect.addFlashAttribute("MENSAJE", "Platillo Actualizado");
-					} else {
-						String x = rt.postForObject(URL + "platillo/registrar", request, String.class);
-						redirect.addFlashAttribute("MENSAJE", "Platillo Registrado");
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			RestTemplate rt = new RestTemplate();
+			// Serializar
+			Gson gson = new Gson();
+			String json = gson.toJson(p);
+			
+			HttpHeaders headers=new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> request=new HttpEntity<String>(json,headers);
+			if(p.getIdPlatillo() == 0) {
+				rt.postForObject(URL+"platillo/registrar", request, String.class);
+				redirect.addFlashAttribute("MENSAJE", "Platillo registrado");
+			}
+			else {
+				rt.put(URL+"platillo/actualizar", request, String.class);
+				redirect.addFlashAttribute("MENSAJE", "Platillo actualizado");
 			}
 		} catch (Exception e) {
 			redirect.addFlashAttribute("MENSAJE", "Error en la grabación");
