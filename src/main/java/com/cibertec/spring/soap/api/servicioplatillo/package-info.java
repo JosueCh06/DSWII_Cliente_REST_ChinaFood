0da9ece78b9bf7a3d790ccb72634f4efb4551f8f
JosuéCh06 <josuechupica06@gmail.com>
2022-06-30 14:42:23 -0500

    Mejoras al cliente

--- src/main/java/com/cibertec/spring/soap/api/servicioplatillo/package-info.java
+++ src/main/java/com/cibertec/spring/soap/api/servicioplatillo/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.06.30 a las 01:59:49 PM PET 
+// Generado el: 2022.06.30 a las 02:22:00 PM PET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.cibertec.com/spring/soap/api/servicioPlatillo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
